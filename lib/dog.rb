be52e15a3715aab3259c6e776f07889761336f93
christopher <cdnance1829@gmail.com>
2022-07-26 16:50:17 -0400

    Complete Assignment

---
+++ lib/dog.rb
@@ -0,0 +1,11 @@
+#! /usr/bin/env/ruby
+
+class Dog 
+    def bark
+        puts "Woof!"
+    end
+
+    def sit 
+        puts "The Dog is sitting"
+    end
+end
---
+++ lib/person.rb
@@ -0,0 +1,11 @@
+#! /usr/bin/env/ruby
+
+class Person
+    def talk
+        puts "Hello World!"
+    end
+
+    def walk
+        puts "The Person is walking"
+    end
+end
